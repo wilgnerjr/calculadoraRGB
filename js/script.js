b7d6adbb3c0ac1c837e763b7c467983f3011edb9
WilgnerJR <wilgnerbento@gmail.com>
2023-02-26 22:57:20 -0400

    calculadora

---
+++ js/script.js
@@ -0,0 +1,38 @@
+let calculadora = document.getElementById('calculadora');
+function insert(num){
+    var numero = document.getElementById('resultado').innerHTML;
+    document.getElementById('resultado').innerHTML = numero + num;
+}
+function limpar(){
+    document.getElementById('resultado').innerHTML = '';
+}
+function voltar(){
+    var resultado = document.getElementById('resultado').innerHTML;
+    document.getElementById('resultado').innerHTML = resultado.substring(0,resultado.length - 1);
+}
+function calcular(){
+    var resultado = document.getElementById('resultado').innerHTML;
+    if (resultado){
+        document.getElementById('resultado').innerHTML = eval(resultado);
+    }else{
+        document.getElementById('resultado').innerHTML = 'Valor Não Encontrado...';
+    }
+}
+
+
+function activeR(){
+    calculadora.classList.remove('activeR','activeG','activeB','activeA');
+    calculadora.classList.add('activeR');
+}
+function activeG(){
+    calculadora.classList.remove('activeR','activeG','activeB','activeA');
+    calculadora.classList.add('activeG');
+}
+function activeB(){
+    calculadora.classList.remove('activeR','activeG','activeB','activeA');
+    calculadora.classList.add('activeB');
+}
+function activeA(){
+    calculadora.classList.remove('activeR','activeG','activeB','activeA');
+    calculadora.classList.add('activeA');
+}
